d736bcb932017eaffbf7365d2e0aae332344c1ea
Ian OCONNOR <ianoconnor@Ians-MacBook-Air.local>
2013-10-15 16:07:05 +0800

    HW2

--- app/scripts/controllers/categories.js
+++ app/scripts/controllers/categories.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('referenceMeApp')
-  .controller('CategoriesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CategoriesCtrl', function ($scope, $rootScope, $routeParams) {
+    $scope.selectedTag = $rootScope.selectedTag;
+
+    $scope.selectedLinks = [];
+    for (var i=0; i < hipchatLinks.length; i++) {
+      if ($scope.selectedTag == hipchatLinks[i].tags) {
+      	$scope.selectedLinks.push(hipchatLinks[i]);
+console.log("$routeParams success?")
+console.log($routeParams.selectedTagInURL);
+$scope.selectedTagThroughRoutes = $routeParams.selectedTagInURL;
+
+      }
+    }
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('referenceMeApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $rootScope, $location) {
+    $scope.hipchatLinks = hipchatLinks; //picks up the source file and $scope makes the variable work across the files
+    $scope.tags = ["Javascript", "Funny", "CSS"]; //these are our tags
+
+	$scope.clickLink = function(tag) {
+		
+		$location.path("/categories");
+		$rootScope.selectedTag = tag;
+	}    
+});
